a33fb3eaa7fa4eb3b7b718b3db8f57ce00dcca99
Your Name <you@example.com>
2025-09-10 05:35:04 +0330

    restructure

--- src/utils/io.js
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs"
-
-/**
- * reads file and returns its content as a string
- * @param {string} path
- */
-export function readFileSync(path) {
-    return fs.readFileSync(path, "utf-8")
-}
